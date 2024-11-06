131f08a9c505d0124d119579b89e9120c03ca21d
faizan-tatamotors <faizans.ss@tatamotors.com>
2024-11-06 11:19:57 +0530

    mileageSarathiSubscriptionPlansSection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MileageSarthiSubscriptionComponent } from './components/mileage-sarthi-subscription/mileage-sarthi-subscription.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MaterialModule } from './Material.Module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MileageSarthiSubscriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
